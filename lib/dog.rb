2066c5f0a93d550901ccccf8a6b20bffd7db2a3b
serkansahin55 <ssahin55@outlook.com>
2020-10-27 11:53:55 -0400

    Day2

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+    def name=(name_of_dog)
+        @name = name_of_dog
+    end
+    def name
+        @name
+    end
+
+    def breed=(breed_of_dog)
+        @breed = breed_of_dog
+    end
+    def breed 
+        @breed
+    end
+end
